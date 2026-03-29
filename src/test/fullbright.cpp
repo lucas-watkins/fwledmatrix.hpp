72cc5f74e86bfb59c334b80082fa9834e2eee48e
lucas-watkins <lucas-watkins@outlook.com>
2026-03-28 21:12:27 -0400

    Good full bright test

--- src/test/fullbright.cpp
+++ src/test/fullbright.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include <chrono>
+#include <fwledmatrix.hpp>
+#include <thread>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    fwledmatrix matrix{"/dev/ttyACM0"};
+    fwledmatrix matrix1{"/dev/ttyACM1"};
+
+    for (int x{0}; x < fwledmatrix::width; ++x) {
+        for (int y{0}; y < fwledmatrix::height; ++y) {
+            matrix.set(x, y, 255);
+            matrix1.set(x, y, 255);
+        }
+    }
+
+    matrix.flush();
+    matrix1.flush();
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
+    matrix.clear();
+    matrix1.clear();
+
+    for (int x{0}; x < fwledmatrix::width; ++x) {
+        for (int y{0}; y < fwledmatrix::height; ++y) {
+            matrix.set(x, y, 0);
+            matrix1.set(x, y, 0);
+        }
+    }
+
+    matrix.flush();
+    matrix1.flush();
+
     return 0;
 }
